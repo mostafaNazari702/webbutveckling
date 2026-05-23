73182f9144199adb40e40471aa1f8a358e4663d7
mostafanazari702 <nazarimostafa2006@gmail.com>
2026-05-23 16:44:45 +0200

    fixat pages problem på github

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/min-hemsida/",
   plugins: [react()],
   server: {
     port: 5173,
-    open: true
+    open: true,
+    allowedHosts: ["localhost", "yourdomain.com", "ranking-giant-chips-www.trycloudflare.com"]
   }
 });
