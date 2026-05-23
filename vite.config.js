e7dbe2d021c69aa60d9b003025dcdf0aa9050209
mostafanazari702 <nazarimostafa2006@gmail.com>
2026-05-23 17:08:39 +0200

    Fixa GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/min-hemsida/",
+  base: "/webbutveckling/",
   plugins: [react()],
   server: {
     port: 5173,
